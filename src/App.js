1b6b352690200939a4084f267281d6ef7880b1f7
Joel Yek <joevo2@gmail.com>
2019-07-30 12:20:09 +0800

    experiment useState with num, string and obj

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Nothing");
+  const [object, setObject] = useState({});
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Add count</button>
+        
+        <p>{name}</p>
+        <button onClick={() => setName("Joel")}>Set name</button>
+    
+        <p>{JSON.stringify(object)}</p>
+        <button onClick={() => setObject({ test: "test" })}>Set object</button>
         <a
           className="App-link"
           href="https://reactjs.org"
